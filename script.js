b63c1e0f9c66560b87f621fc74669459a5f101ef
Andrea <65364798+beandrea@users.noreply.github.com>
2020-12-02 19:46:45 -0500

    icon

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
+    $("#button-addon2").on("click", function() {
+        let pas = $("input").val();
+        
+        let tot = 0;
+        for(let i = 0; i < pas.length; i++){
+            tot += pas.charCodeAt(i);
+        }
 
+        let shift = tot % pas.length;
+        var newStr = "";
+        for(let i = 0; i < pas.length; i++){
+            let singleChrBits = pas.charCodeAt(i);
+            let negVal = ~singleChrBits;
+            for(let j = 0; j < 3; j++){
+                singleChrBits = singleChrBits >> shift;
+                singleChrBits = singleChrBits ^ (keyVar + j);
+                singleChrBits = singleChrBits ^ negVal
+                singleChrBits = abs(single ^ shift);
+            }
+            newStr += String.fromCharCode(singleChrBits);
+        }
+        console.log(newStr);
+    });
 });
